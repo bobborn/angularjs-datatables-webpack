caf14d68f3b7fa37a3d152809a080a685f13b9bc
wersdxzfer <514899126@qq.com>
2016-01-25 11:24:37 +0800

    role routes

--- src/components/role/role.routes.js
+++ src/components/role/role.routes.js
@@ -2,8 +2,13 @@
  * Created by bob on 2016/1/20.
  */
 
-
 function routes($stateProvider){
+    $stateProvider.state('roleListController', {
+        url: '/role-list',
+        templateUrl: './components/role/role.tpl.html',
+        controller: 'RoleListController',
+        controllerAs: 'rl'
+    })
 }
 
 routes.$inject=['$stateProvider'];
